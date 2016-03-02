55ea889c40c481f4ac0d143bcfea1008335ae25a
AdamsBenjamin <adamsbt@appstate.edu>
2016-03-01 19:34:00 -0500

    appeasing stephen

--- brackets.py
+++ brackets.py
@@ -1,27 +1,28 @@
-#!/usr/bin/env python
+#/usr/bin/env python
+
+import numpy as np
+import json
+import os
+import sys
 
 __author__ = 'Benjamin Adams'
 __version__ = '0.1'
 __email__ = 'adamsbt@appstate.edu'
 __credit__ = 'Benjamin Adams, Jenna Schachner, Gabriel Mercer'
 
-import pprint as pp
-import numpy as np
-import json
-import os
-import sys
-
 def parse_stats(filename):
-	#checks that the given file exists and is a valid format
-	if not os.path.exists(filename) or not filename.endswith('.json'):
+	#checks that the given file exists and is a valid format.
+	if not os.path.isfile(filename) or not filename.endswith('.json'):
 		print('Invalid path or file type.')
 		sys.exit(1)
 
-	#parses and saves the file data to a variable
+	#parses and saves the file data to an array.
 	data = []
 	with open(filename, 'r') as fp:
 		data = json.load(fp)
 
+	#returns a tuple of the array of dictionaries parsed from the json file
+	#as well as a count of the number of teams.
 	return (data, len(set(entry['home']['team'] for entry in data)))
 
 
@@ -33,20 +34,23 @@ def generate_matrices(data, num_teams):
 
 	#single-pass through all dictionaries in data array
 	for entry in data:
+		#checks that both teams have been assigned column numbers.
+		#those that have not are assigned the next largest number.
 		if entry['home']['team'] not in team_index.keys():
 			team_index[entry['home']['team']] = team_counter
-			#print(entry['home']['team'] + ': ' + str(team_index[entry['home']['team']]))
 			team_counter += 1
-
 		if entry['away']['team'] not in team_index.keys():
 			team_index[entry['away']['team']] = team_counter
-			#print(entry['away']['team'] + ': ' + str(team_index[entry['away']['team']]))
 			team_counter += 1
 
+		#sets the home and away team flags for the most recently
+		#indexed game.
 		M.append([0 for i in range(num_teams)])
 		M[-1][team_index[entry['home']['team']]] = 1
 		M[-1][team_index[entry['away']['team']]] = -1
 
+		#adds the newest game's point difference, in terms of the
+		#home team, to the end of the y vector.
 		y.append(entry['home']['pts'] - entry['away']['pts'])
 
 	M.append([1 for i in range(num_teams)])
@@ -55,8 +59,8 @@ def generate_matrices(data, num_teams):
 	return (np.array(M), np.array(y), team_index)
 
 
-def rate_teams()
-	pass
+def rate_teams(data, team_index):
+	for item, in zip(data, range(1, len(data)+1)):
 
 
 data = []
@@ -69,5 +73,5 @@ else:
 M,y,team_index = generate_matrices(data, num_teams)
 
 r = np.linalg.lstsq(M, y)[0]
-print(r)
-
+#print(r)
+rate_teams(r, team_index)
