8580f445006a158e768b924b2632f6e99363cf34
AdamsBenjamin <adamsbt@appstate.edu>
2016-03-02 09:01:14 -0500

    fixed home team advantage. added documentation.

--- brackets.py
+++ brackets.py
@@ -52,7 +52,11 @@ def generate_matrices(data, num_teams):
 
 		# adds the newest game's point difference, in terms of the
 		# home team, to the end of the y vector.
-		y.append(entry['home']['pts'] - entry['away']['pts'] - 3)
+		y.append(entry['home']['pts'] - entry['away']['pts'])
+
+		#home team advantage correction
+		if entry['home']['pts'] > entry['away']['pts']:
+			y[-1] -= 2
 
 	M.append([1 for i in range(num_teams)])
 	y.append(0)
@@ -62,11 +66,13 @@ def generate_matrices(data, num_teams):
 
 def rate_teams(data, team_index):
 	columns = [None for i in range(len(data))]
+
+	#place the team names into array at index matching their unique number
 	for i in team_index.keys():
 		columns[team_index[i]] = i
 	ranks = zip(columns, data)
 	ranks = list(reversed(sorted(ranks, key = lambda i: i[-1])))
-	for i in range(50):
+	for i in range(len(ranks)):
 		print(str(i+1) + ' ' + str(ranks[i][0]) + ' {:.2f}'.format(ranks[i][1]))
 
 
